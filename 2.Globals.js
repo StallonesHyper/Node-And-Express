5e44e3c52b42bb00262f25fff26f1bb7a27c82b4
StallonesHyper <stallonefernandess@gmail.com>
2022-07-10 18:35:38 +0530

    Intro Node JS

---
+++ 2.Globals.js
@@ -0,0 +1,15 @@
+// Globals
+
+/*
+__dirname = Path To Current Directory 
+__filename = File Name 
+require = Import Modules
+module = Describes The Module 
+process = Information About ENV 
+*/
+
+console.log(__dirname);
+
+setInterval(() => {
+  console.log("Hi");
+}, 3000);
